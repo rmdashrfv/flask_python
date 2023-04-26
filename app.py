d16a61dc7da67e448051d341e1f540ee56c35766
Michael Law <rmdashrfv@hey.com>
2023-04-26 10:21:26 -0400

    Start without login

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
 from flask import Flask, send_file, request, jsonify
-from utils import authenticate, create_access_token
 from flask_migrate import Migrate
 from flask_cors import CORS
 from config import Config
 from models import *
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 import platform
 
 app = Flask(__name__, static_folder='public')
@@ -42,22 +41,7 @@ def users():
     return jsonify(user.to_dict()), 201
 
 
-@app.post('/login')
-def login():
-    data = request.form
-    user = User.query.filter_by(email=data.get('email')).first()
-    if not user:
-        return jsonify({'error': 'No user found'}), 404
-    given_password = data.get('password')
-    if user.password == given_password:
-        token = create_access_token(user.id)
-        return jsonify({'user': user.to_dict(), 'token': token})
-    else:
-        return jsonify({'error': 'Invalid email or password'}), 422
-
-
 @app.get('/users/<int:id>')
-@authenticate
 def show(id):
     user = User.query.get(id)
     if user:
@@ -82,7 +66,6 @@ def update_user(id):
 
 
 @app.delete('/users/<int:id>')
-@authenticate
 def delete_user(id, current_user):
     user = User.query.get(id)
     if user:
@@ -100,27 +83,6 @@ def delete_user(id, current_user):
 def show_post(id):
     post = Post.query.get(id)
     return jsonify(post.to_dict())
-
-
-@socketio.on('connect')
-def connected():
-    '''This function is an event listener that gets called when the client connects to the server'''
-    print(f'Client {request.sid} has connected')
-    emit('connect', {'data': f'id: {request.sid} is connected'})
-
-
-@socketio.on('data')
-def handle_message(data):
-    '''This function runs whenever a client sends a socket message to be broadcast'''
-    print(f'Message from Client {request.sid} : ', data)
-    emit('data', {'data': data, 'id': request.sid}, broadcast=True)
-
-
-@socketio.on("disconnect")
-def disconnected():
-    '''This function is an event listener that gets called when the client disconnects from the server'''
-    print(f'Client {request.sid} has disconnected')
-    emit('disconnect', f'Client {request.sid} has disconnected', broadcast=True)
 
 
 if __name__ == '__main__':
